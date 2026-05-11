04948a168685e0e2de5507eb9f155fe21c73c864
banikyl-source <banikyl@gmail.com>
2026-05-11 17:38:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import asyncio
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 from receipt_pdf_bot.bot import main
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
