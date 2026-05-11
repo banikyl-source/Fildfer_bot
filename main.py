646b812a8185716c1640756dcc0d533c268862b6
banikyl-source <banikyl@gmail.com>
2026-05-11 20:54:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import asyncio
+import sys
+from pathlib import Path
+
+# Добавляем папку src в путь поиска модулей
+sys.path.insert(0, str(Path(__file__).parent / "src"))
+
 from receipt_pdf_bot.bot import main
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
