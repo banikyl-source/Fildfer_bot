0c2beef01b2efab8a80c74b114bc79c3189017b3
banikyl-source <banikyl@gmail.com>
2026-06-08 18:17:23 +0500

    Delete src/receipt_pdf_bot directory

--- src/receipt_pdf_bot/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Demo receipt PDF generator (with mandatory ОБРАЗЕЦ watermark)."""
-
-from receipt_pdf_bot.receipt import ReceiptData, render_receipt_pdf
-
-__all__ = ["ReceiptData", "render_receipt_pdf"]
